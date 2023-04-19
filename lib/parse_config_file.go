b40ce418b97078e28f030a0414e53fc6e8f1f2db
jiaruling@libii.com <jiaruling@libii.com>
2023-04-19 14:38:33 +0800

    ✨ feat(parse_config_file): 新增配置文件解析方式

--- lib/parse_config_file.go
+++ lib/parse_config_file.go
@@ -2,13 +2,14 @@ package lib
 
 import (
 	"fmt"
+	"path/filepath"
+	"strings"
 
 	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/viper"
 )
 
 // 加载配置文件并进行监听
-
 func ParseConfig(path string, obj interface{}, isWatch bool) (err error) {
 	// 读取配置文件，映射到结构体
 	// 实例化viper对象
@@ -35,6 +36,55 @@ func ParseConfig(path string, obj interface{}, isWatch bool) (err error) {
 				fmt.Println("配置文件发生变化:", e.Name)
 				_ = v.ReadInConfig() // 重新读取配置数据
 				_ = v.Unmarshal(obj) // 将文件内容映射到结构体
+			})
+		}()
+	}
+	return
+}
+
+var ViperConfMap map[string]*viper.Viper
+
+func ParseConfigViper(path, suffix string, isWatch bool) (err error) {
+	// 创建一个新的 viper 实例
+	v := viper.New()
+
+	// 设置配置文件的类型
+	v.SetConfigType(suffix)
+
+	// 设置配置文件的路径
+	v.SetConfigFile(path)
+
+	// 读取配置文件
+	if err := v.ReadInConfig(); err != nil {
+		return err
+	}
+
+	// 放入ViperConfMap中
+	_, file := filepath.Split(path)
+	fileName := strings.Split(file, ".")[0]
+	if ViperConfMap == nil {
+		ViperConfMap = make(map[string]*viper.Viper)
+	}
+	ViperConfMap[fileName] = v
+
+	if isWatch {
+		// viper的功能 -- 动态监控变化
+		fmt.Println("开启配置 " + path + " 文件监听")
+		go func() {
+			// 开启监听功能
+			v.WatchConfig()
+			// 文件监听
+			v.OnConfigChange(func(e fsnotify.Event) {
+				// 打印变换的文件名
+				fmt.Println("配置文件发生变化:", e.Name)
+				err := v.ReadInConfig() // 重新读取配置数据
+				if err != nil {
+					return
+				} else {
+					_, file := filepath.Split(e.Name)
+					fileName := strings.Split(file, ".")[0]
+					ViperConfMap[fileName] = v
+				}
 			})
 		}()
 	}
